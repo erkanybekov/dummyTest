7126387ed75acca23dd2175e048b1bc1653e4352
Erlanbek <kanybekov668@gmail.com>
2025-05-24 22:06:21 +0600

    hi

--- dummyTest/ContentView.swift
+++ dummyTest/ContentView.swift
@@ -8,15 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var text:String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Enter text", text: $text )
+                .frame(alignment: .center)
+                .background(Color.yellow)
+                .foregroundColor(.black)
+                .frame(width: .infinity, height: 32)
+                .clipShape(.rect(cornerRadius: 16))
+                .padding(.bottom, 16)
+                .padding(.horizontal, 16)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color.yellow, lineWidth: 1)
+                )
+                .textInputAutocapitalization(TextInputAutocapitalization.never)
+
+            
         }
+        .ignoresSafeArea(.all)
         .padding()
+        
+        Text(text)
+        
+        switch text {
+        case "":
+            Text("Empty")
+        case "swift":
+            Text("yeah nigga")
+        case "kotlin":
+            Text("fuck ya go back for ya kmp")
+        default:
+            Text("Not Empty")
+        }
+        
     }
+        
 }
 
 #Preview {
