928bf0abd32d8aed7a932b842102fa4f67464cba
Erlanbek <kanybekov668@gmail.com>
2025-05-24 19:11:21 +0600

    Initial Commit

---
+++ dummyTest/dummyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  dummyTestApp.swift
+//  dummyTest
+//
+//  Created by Erlan Kanybekov on 5/24/25.
+//
+
+import SwiftUI
+
+@main
+struct dummyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
